6c631c5fd7a1a5c88357e0762d51bd2b7ca05b10
NakulSiwach <Nakul.Siwach@iiitb.ac.in>
2024-09-21 16:09:26 +0530

    temp

---
+++ 23.c
@@ -0,0 +1,13 @@
+/*
+
+Name : Nakul Siwach
+Roll No : MT2024096
+Program : 23. Write a program to print the maximum number of files can be opened within a process and
+size of a pipe (circular buffer).
+*/
+
+
+    
+/*
+
+*/
